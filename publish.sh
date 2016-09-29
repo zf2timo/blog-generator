36c93e29b02582dfd53d9b00a455c932125be189
zf2timo <timo.tewes@gmail.com>
2016-09-29 21:04:25 +0200

    publish scripts executes complete deployment

--- publish.sh
+++ publish.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+
+if [ ! -d "build" ]; then
+    git clone git@github.com:zf2timo/zf2timo.github.io.git build
+fi
+
+cd build
+git pull origin master
+cd -
+
 # Replace "sculpin generate" with "php sculpin.phar generate" if sculpin.phar
 # was downloaded and placed in this directory instead of sculpin having been
 # installed globally.
@@ -9,9 +18,9 @@ if [ $? -ne 0 ]; then echo "Could not generate the site"; exit 1; fi
 
 # Add --delete right before "output_prod" to have rsync remove files that are
 # deleted locally from the destination too. See README.md for an example.
-rsync -avze --delete output_prod/ docs/
+rsync -avze --delete output_prod/ build/
 if [ $? -ne 0 ]; then echo "Could not publish the site"; exit 1; fi
 
-git add docs/
-git commit -m"Updated Blog"
+git add build/
+git commit -m"Updated Blog at" `date +"%Y-%m-%d %H:%M:%S"`
 git push origin master
